79d94d287a98ca41fa3757cf5adb5cce9d453e7b
1337Dany <dmunatayev.dev@gmail.com>
2025-06-12 22:47:36 +0200

    init

--- src/DTOs/DriverDetailedDTO.cs
+++ src/DTOs/DriverDetailedDTO.cs
@@ -1,6 +1,17 @@
-﻿namespace DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DTOs;
 
 public class DriverDetailedDTO
 {
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime Birthday { get; set; }
     
+    public int CarNumber { get; set; }
+    public string CarManufacturerName { get; set; }
+    public string CarModelName { get; set; }
 }
